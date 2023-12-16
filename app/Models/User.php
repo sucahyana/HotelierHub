eaa03d622a7f9d2ae754057d97eed9d79775b825
Sucahyana <sucahyana15@gmail.com>
2023-12-17 01:35:01 +0700

    migration and model adding role

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($roleName)
+    {
+        return $this->roles()->where('name', $roleName)->exists();
+    }
     protected static function boot()
     {
         parent::boot();
